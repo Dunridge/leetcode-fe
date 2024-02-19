acec23ea4ff3a466684968168a34b5da8b6aeaca
Dunridge <Polinka7max@gmail.com>
2024-02-18 21:10:12 -0800

    develop. optimize bfs

--- src/components/Algorithms.tsx
+++ src/components/Algorithms.tsx
@@ -4,45 +4,44 @@ import { IGraph } from "../utils/interfaces/GraphType";
 import 'vis-network/styles/vis-network.css';
 import GraphVisualization from "./GraphVisualization ";
 
+// TODO: find and solve FE tasks masked as dfs/bfs (this is what is given at interviews)
 export default function Algorithms() {
     const [graph, setGraph] = useState<IGraph>(graphAdjacencyObj);
+    // TODO: expose the current code for the methods on the website and then deploy this on netlify
 
-    const dfs = (node: number, visited: Set<number> = new Set()): void => {
+    // TODO: keep the code short and try to minize the number of printed dfs results - the algorithm should print out the result once
+    const dfs = (node: number, visited: Set<number> = new Set()): any => {
         if (visited.has(node)) return;
         visited.add(node);
-        console.log(node);
-        if(!graph[node]) return;
         graph[node]?.forEach(neighbor => dfs(neighbor, visited));
+        // TODO: get rid of the bug where it's called three times (so that you can translate it to problems)
+        visited.size === Object.keys(graph).length && console.log('dfs: ', visited); 
     }
 
-    const bfs = (start: number): void => {
-        const queue: number[] = [start];
-        const visited: Set<number> = new Set([start]);
-
-        while (queue.length > 0) {
-            const current: number = queue.shift()!;
-            console.log(current);
-
-            if (graph[current]) {
-                for (const neighbor of graph[current]) {
-                    if (!visited.has(neighbor)) {
-                        visited.add(neighbor);
-                        queue.push(neighbor);
-                    }
-                }
-            }
-        }
+    // TODO: get rid of the initial queue arr and replace with the node number
+    const bfs = (queue: number[], visited: Set<number> = new Set()): void => {
+        if (queue.length === 0) {
+            console.log('bfs: ', visited);
+            return;
+        };
+        const current = queue.shift()!;
+        graph[current]?.forEach(neighbor => {
+            if (visited.has(neighbor)) return;
+            visited.add(neighbor);
+            queue.push(neighbor);
+        });
+        bfs(queue, visited);
     };
 
     return (
         <div>
             <h2>Algorithms</h2>
             <h2>Graph</h2>
-            <GraphVisualization adjacencyList={graph}/>
+            <GraphVisualization adjacencyList={graph} />
             <hr />
-            <button onClick={() => dfs(1)}>DFS, Node 1</button>
+            <button onClick={() => dfs(1)}>DFS</button>
             <hr />
-            <button onClick={() => bfs(1)}>BFS, Node 1</button>
+            <button onClick={() => bfs([1])}>BFS</button>
         </div>
     );
 };
