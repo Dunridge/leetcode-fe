79427249ec0efa74cac7c353925c7e902d7b63b6
Dunridge <Polinka7max@gmail.com>
2024-02-18 21:44:54 -0800

    develop. add callback

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { parse, Options } from 'acorn';
@@ -6,21 +6,21 @@ import { parse, Options } from 'acorn';
 const CodeDisplay = ({ func }: any) => {
   const [code, setCode] = useState('');
 
-  const parseCode = () => {
+  const parseCode = useCallback(() => {
     try {
       const codeString = func.toString();
-      const options: Options = { sourceType: 'module', ecmaVersion: 2022 }; // Specify the ECMAScript version
+      const options: Options = { sourceType: 'module', ecmaVersion: 2022 };
       const parsedCode = parse(codeString, options);
       const functionCode = parsedCode.body.find((node) => node.type === 'FunctionDeclaration');
       setCode(codeString.substring(functionCode?.start, functionCode?.end));
     } catch (error) {
       console.error('Error parsing code:', error);
     }
-  };
+  }, [func]);
 
   useEffect(() => {
-    parseCode();
-  }, [parseCode]);
+    parseCode(); // Call parseCode directly inside the useEffect
+  }, [parseCode]); // Include parseCode in the dependency array
 
   return (
     <SyntaxHighlighter language="javascript" style={solarizedlight}>
